c868e64ab743d7acd463cd8649a2da37f1794a9f
Juan del Moral <jdelm86@gmail.com>
2023-05-12 23:33:32 -0300

    trabajo-final

--- ecommerce/src/components/ItemDetail/ItemDetail.js
+++ ecommerce/src/components/ItemDetail/ItemDetail.js
@@ -1,8 +1,15 @@
+import { useState } from "react"
 import ItemCount from "../ItemCount/ItemCount"
+import { Link } from "react-router-dom"
 
 
 
 const ItemDetail = ({id, name, img, category, description, price, stock}) => {
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+    }
     return (
         <article className="CardItem">
             <header className="Header">
@@ -25,7 +32,14 @@ const ItemDetail = ({id, name, img, category, description, price, stock}) => {
                 </p>
             </section>
             <footer className="ItemFooter">
-                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log("cantidad agregada")} />
+                {
+                    quantityAdded > 0 ? (
+                        <Link to='/cart' className="Option">Terminar Compra</Link> 
+                    ) : (
+                        <ItemCount initial={1} stock={stock} onAdd={handleOnAdd}/>
+                    )
+                }
+            
             </footer>
         </article>
     )
