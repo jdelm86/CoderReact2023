4b6446da2568a70a0548b4a3df5d51946afd9f9d
Juan del Moral <jdelm86@gmail.com>
2023-05-18 23:50:31 -0300

    agregando componentes antes de Firebase

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -5,26 +5,28 @@ import './components/components.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
 
-  function App() {
-    return (
-      <div className="App">
-  <BrowserRouter>
-  <NavBar />
-  <Routes>
-    <Route path='/' element={<ItemListContainer />}/>
-    <Route path='/category/:categoryId' element={<ItemListContainer />}/>
-    <Route path='/item/:itemId' element={<ItemDetailContainer />}/>
-    <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
-  </Routes>
-  </BrowserRouter>
-      </div>
-    );
-  }
+import { CartProvider } from './components/Context/CartContext';
 
-
-
-
+function App() {
+  return (
+    <div className="App">
+<BrowserRouter>
+    <CartProvider>
+    <NavBar /> 
+      <Routes>
+          <Route path='/' element={<ItemListContainer />}/>
+          <Route path='/category/:categoryId' element={<ItemListContainer />}/>
+          <Route path='/item/:itemId' element={<ItemDetailContainer />}/>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
+      </Routes>
+    </CartProvider>
+</BrowserRouter>
+</div>
+  );
+}
 
 
 export default App;
--- ecommerce/src/components/ItemDetail/ItemDetail.js
+++ ecommerce/src/components/ItemDetail/ItemDetail.js
@@ -1,14 +1,24 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import ItemCount from "../ItemCount/ItemCount"
 import { Link } from "react-router-dom"
+import { CartContext } from "../Context/CartContext"
 
 
 
 const ItemDetail = ({id, name, img, category, description, price, stock}) => {
     const [quantityAdded, setQuantityAdded] = useState(0)
 
+    const { addItem } = useContext(CartContext)
+
+    
     const handleOnAdd = (quantity) => {
         setQuantityAdded(quantity)
+
+        const item = {
+            id, name, price
+        }
+
+        addItem(item, quantity)
     }
     return (
         <article className="CardItem">
